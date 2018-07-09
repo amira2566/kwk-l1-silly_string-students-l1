641771c88a8135ea94072d9b3b36a300967d33b7
amira2566 <braemira2@icloud.com>
2018-07-09 02:04:19 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -2,3 +2,4 @@ puts "supercalifragilisticexpadlidocious".upcase
 puts "supercalifragilisticexpialidocious".length
 puts "supercalifragilisticexpialidocious"+"and"+"fire"
 puts "i really like programming"
+puts 
