26b7ebde9f213b588d1f80e421565d9d5a16a149
amira2566 <braemira2@icloud.com>
2018-07-09 02:04:39 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -2,4 +2,4 @@ puts "supercalifragilisticexpadlidocious".upcase
 puts "supercalifragilisticexpialidocious".length
 puts "supercalifragilisticexpialidocious"+"and"+"fire"
 puts "i really like programming"
-puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!
+puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
