68b667044e53b1e2d7042f22df74c2724ae2f71f
amira2566 <braemira2@icloud.com>
2018-07-09 02:09:33 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,8 +1,8 @@
-def favorite_word
+def your_favorite_word
 puts "supercalifragilisticexpadlidocious".upcase
 puts "supercalifragilisticexpialidocious".length
 puts "supercalifragilisticexpialidocious"+"and"+"fire"
 puts "i really like programming"
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".reverse
 end
-favorite_word
+your_favorite_word
